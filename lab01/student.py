ac03b3c4d6d590e324079f30c68bd47c739c06f4
JohnpaulBarreto <c00325570@setu.ie>
2026-09-14 10:53:08 +0000

    Complete lab01

---
+++ lab01/student.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+
+print("Hello, " + name + "!") 
+
+studentNumber = input("What is your student Number? ")
+
+print("Your studentNumber is " + studentNumber + "!") 
+
+Year = input("What is your year of study"  )
+
+print("Your year of study is " + Year + "!" )
+
+
